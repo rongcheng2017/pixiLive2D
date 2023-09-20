62f9e35306e34de7de661b34e93e7f881a922468
frc <fengrongcheng@huoban.ai>
2023-09-20 21:08:49 +0800

    add:添加侧边爬墙、强制结束前一个动画等操作

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <div >
-    <!-- style="position: absolute; top: 0; left: 0;" -->
     <canvas ref="liveCanvas" style="position: absolute; top: 0; left: 0;" />
-    <!-- <button @click="expressHappy">开心</button>
-    <button @click="leftcombilewall">左爬墙</button>
-    <button @click="rightcombilewall">右爬墙</button>
-    <button @click="drag">拽起</button>
-    <button @click="jumpOut">跳出</button>
-    <button @click="jumpBack">调回</button>
-    <button @click="waveDance">挥手舞</button>
-    <button @click="hearing">聆听</button>
-    <button @click="addExpression">添加表情</button>
-    <button @click="remove">取消表情</button>
-    <button @click="idle">空闲</button>
-    <button @click="talk">说话</button> -->
   </div>
 </template>
 <script>
 import * as PIXI from 'pixi.js'
 import { Live2DModel } from 'pixi-live2d-display/cubism4'
+import { MotionPriority } from 'pixi-live2d-display';
 
 window.PIXI = PIXI // 为了pixi-live2d-display内部调用
 
 let app // 为了存储pixi实例
-let model // 为了存储live2d实例
+export let model // 为了存储live2d实例
 
 export default {
   async mounted() {
@@ -39,7 +27,7 @@ export default {
     //app.renderer.backgroundColor = 0x061698;
     // 打包后live2d资源会出现在dist/下，这里用相对路径就能引用到了
     model = await Live2DModel.from('./Nika/NIKA.model3.json')
-    //model = await Live2DModel.from('./Mao/Mao.model3.json')
+    //model = await Live2DModel.from('https://download.lynksoul.com/models/Nika.zip')
     model.x =-55;
     model.y =-80;
     //app.renderer.view.with=500;
@@ -47,10 +35,11 @@ export default {
     app.renderer.view.style.position = "absolute";
     app.renderer.view.style.display = "block";
     app.renderer.autoResize = true;
-     app.renderer.resize(window.innerWidth, window.innerHeight);
+    app.renderer.resize(window.innerWidth, window.innerHeight);
     app.stage.addChild(model)
     model.scale.set(0.045) // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
     model.on('hit', hitAreas => {
+      console.log("触发点击区域");
       if (hitAreas.includes('Head')) {
         console.log("------->")
        // model.motion('touch_head')
@@ -61,56 +50,7 @@ export default {
     });
   },
   methods: {
-    expressHappy() {
-      // 调用Live2D模型的表情方法，这里你可以根据需要传递不同的表情类型
-      model.motion('TapHead',0)
-      console.log("TapHead");
-    },
-    leftcombilewall() {
-      // 调用Live2D模型的表情方法，这里你可以根据需要传递不同的表情类型
-      model.motion('LeftClimbWall')
-      console.log("leftcombilewall");
-    },
-    rightcombilewall() {
-      // 调用Live2D模型的表情方法，这里你可以根据需要传递不同的表情类型
-      model.motion('RightClimbWall')
-      console.log("rightcombilewall");
-    },
-    drag() {
-      // 调用Live2D模型的表情方法，这里你可以根据需要传递不同的表情类型
-     // model.motion('Drag',undefined, MotionPriority.FORCE);
-      model.motion('Drag',0);
-      console.log("drag");
-    },
-    jumpBack() {
-      model.motion("JumpBack");
-    },
-    jumpOut() {
-      model.motion("JumpOut");
-    },
-    waveDance(){
-      model.motion("WavingDance");
-     // model.expression("sleep");
-    },
-    hearing(){
-      console.log("Hearing---->");
-      model.motion("Hearing");
-    },
-    addExpression(){
-      console.log("add expression");
-      model.expression("sleep");
-    },
-    remove(){
-      model.expression();
-    },
-    idle(){
-      console.log("idle-->");
-      model.motion("Idle");
-    },
-    talk(){
-      console.log("talking-->");
-      model.motion("Saying")
-    }
+    
   }
 }
 //静止右击
@@ -119,17 +59,21 @@ document.addEventListener("contextmenu", (event) => {
          console.log("右击---->");
          window.chrome.webview.hostObjects.csobj.RightClick();
       });
-
+let currentMotion = '';
 window.chrome.webview.addEventListener('message', arg => {
-    //document.querySelector(".outer").innerHTML = arg.data.color;
-    //model.motion("JumpBack");
-    console.log(arg.data);
-    //debugger
-    var motionExt=arg.data['motion'];
+
+  var motionExt=arg.data['motion'];
     if (motionExt) {
       console.log("执行动画 ： "+motionExt);
-
+      if(motionExt=='DragUp'&&(currentMotion.includes('RIGHT')||currentMotion.includes('LEFT'))){
+        model.motion('Reset',undefined,MotionPriority.FORCE);
         model.motion(motionExt);
+      }else{
+        
+        model.motion(motionExt,undefined,MotionPriority.FORCE);
+      }
+      currentMotion= motionExt;
+        //model.motion(motionExt);
     }
     var exp = arg.data['expression'];
     if(!exp){
@@ -138,7 +82,6 @@ window.chrome.webview.addEventListener('message', arg => {
     console.log("执行表情 ： "+exp);
     model.expression(exp);
   }
-    
   );      
 
 
