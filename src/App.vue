5d3c236d25e1ce2e2f19c8879f0cfe00d3398304
frc <fengrongcheng@huoban.ai>
2023-09-25 16:50:05 +0800

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <!-- style="position: absolute; top: 0; left: 0"  -->
-    <canvas ref="liveCanvas" style="position: absolute; top: 0; left: 0" />
-    <!-- <button @click="leftcombilewall">左爬墙</button>
+    <canvas ref="liveCanvas"  />
+    <button @click="leftcombilewall">左爬墙</button>
     <button @click="leftcombilewallsaying">左爬墙说</button>
     <button @click="leftcombilewallboring">左爬墙无聊</button>
     <button @click="leftcombilewallhearing">左爬墙听</button>
-    <button @click="leftcombilewallreset">左爬重置</button>
     <button @click="rightcombilewall">右爬墙</button>
     <button @click="rightcombilewallsaying">右爬墙说</button>
     <button @click="rightcombilewallboring">右爬墙无聊</button>
     <button @click="rightcombilewallhearing">右爬墙听</button>
-    <button @click="rightcombilewallreset">右爬重置</button>
     <button @click="headreset">头部动画重置</button>
     <button @click="motionreset">动画重置</button>
     <button @click="changModel">切换模型</button>
@@ -21,7 +19,7 @@
     <button @click="jumpback">跳回去</button>
     <button @click="angry">生气</button>
     <button @click="happy">开心</button>
-    <button @click="expreset">表情重置</button> -->
+    <button @click="expreset">表情重置</button>
   </div>
 </template>
 <script>
@@ -39,7 +37,7 @@ export default {
     // 获取URL参数
     const queryParams = new URLSearchParams(window.location.search)
     // 获取名为 "characterid" 的查询参数的值
-    characterId = queryParams.get('characterid')
+    characterId = queryParams.get('characterid')??1
     console.log('character id')
     console.log(characterId)
     app = new PIXI.Application({
@@ -55,105 +53,70 @@ export default {
     //app.renderer.backgroundColor = 0x061698;
     // 打包后live2d资源会出现在dist/下，这里用相对路径就能引用到了
     loadMode(characterId);
-    //model = await Live2DModel.from('./sealion/hgkoazarasi.model3.json')
-
-    //model = await Live2DModel.from('https://download.lynksoul.com/models/Nika.zip')
-
-    //app.renderer.view.with=500;
-    //app.renderer.view.height=1000;
-
-    // app.stage.addChild(model)
   },
   methods: {
-    async changModel(newId) {
-      // if(newId==characterId) return;
-      // //app.stage.removeChildren()
-      // // let characterName = 'NIKA'
-      // // if (characterId == 1) {
-      // //   characterId = 2
-      // // } else {
-      // //   characterId = 1
-      // // }
-      // if (characterId == 1) {
-      //   characterName = 'NEKO'
-      // } else if (characterId == 2) {
-      //   characterName = 'NIKA'
-      // }
-      // let newModel = await Live2DModel.from(`./${characterName}/${characterName}.model3.json`)
-      // newModel.once('ready', () => {
-     
-      //   console.log('模型准备完成')
-      //   newModel.x = -55
-      //   newModel.y = -80
-      //   app.renderer.view.style.position = 'absolute'
-      //   app.renderer.view.style.display = 'block'
-      //   app.renderer.autoResize = true
-      //   app.renderer.resize(window.innerWidth, window.innerHeight)
-      //   app.stage.removeChild(model)
-      //   app.stage.addChild(newModel)
-      //   newModel.scale.set(0.045) // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
-      //   newModel.motion('JUMP_OUT')
-      //   model = newModel
-      //   newModel = null
-      //   console.log('切换模型')
-      // });
-     
-     
+     changModel() {
+      if(characterId==1) {
+        characterId=2
+      }else{
+        characterId=1;
+      }
+      app.stage.removeChild(model)
+      loadMode(characterId)
     },
     leftcombilewall() {
-      model.motion('LEFT')
+      model.motion('LEFT',undefined, MotionPriority.FORCE)
     },
     leftcombilewallhearing() {
-      model.motion('LEFT_HEARING')
+      model.motion('LEFT_HEARING',undefined, MotionPriority.FORCE)
     },
     leftcombilewallsaying() {
-      model.motion('LEFT_SAYING')
+      model.motion('LEFT_SAYING',undefined, MotionPriority.FORCE)
     },
     leftcombilewallboring() {
-      model.motion('LEFT_BORING')
+      model.motion('LEFT_BORING',undefined, MotionPriority.FORCE)
     },
     leftcombilewallreset() {
-      model.motion('LEFT_RESET')
+      model.motion('LEFT_RESET',undefined, MotionPriority.FORCE)
     },
     rightcombilewall() {
-      model.motion('RIGHT')
+      model.motion('RIGHT',undefined, MotionPriority.FORCE)
     },
     rightcombilewallhearing() {
-      model.motion('RIGHT_HEARING')
+      model.motion('RIGHT_HEARING',undefined, MotionPriority.FORCE)
     },
     rightcombilewallsaying() {
-      model.motion('RIGHT_SAYING')
+      model.motion('RIGHT_SAYING',undefined, MotionPriority.FORCE)
     },
     rightcombilewallboring() {
-      model.motion('RIGHT_BORING');
+      model.motion('RIGHT_BORING',undefined, MotionPriority.FORCE);
     },
     rightcombilewallreset() {
-      model.motion('RIGHT_RESET');
+      model.motion('RIGHT_RESET',undefined, MotionPriority.FORCE);
     },
     motionreset() {
-      model.motion('RESET')
+      model.motion('RESET',undefined, MotionPriority.FORCE)
     },
     saying() {
-      model.motion('CENTER_SAYING')
+      model.motion('CENTER_SAYING',undefined, MotionPriority.FORCE)
     },
     hearing() {
-      model.motion('CENTER_HEARING')
+      model.motion('CENTER_HEARING',undefined, MotionPriority.FORCE)
     },
     jumpback() {
-      model.motion('JUMP_BACK')
+      model.motion('JUMP_BACK',undefined, MotionPriority.FORCE)
     },
     jumpout() {
-      model.motion('JUMP_OUT')
-      // model._startMotion()
+      model.motion('JUMP_OUT',undefined, MotionPriority.FORCE)
     },
     headreset(){
-      model.motion('HEAD_RESET')
+      model.motion('HEAD_RESET',undefined, MotionPriority.FORCE)
     },
     happy(){
-      model.expression('HAPPY')
+      model.expression('HAPPY',undefined, MotionPriority.FORCE)
     },
     angry(){
-      model.expression('ANGRY')
+      model.expression('ANGRY',undefined, MotionPriority.FORCE)
     },
     expreset(){
       model.expression('EXP_RESET')
@@ -172,12 +135,12 @@ function loadMode(modelId){
    model =  Live2DModel.fromSync(modelJson)
         model.once('ready', () => {
           console.log('模型准备完成')
-          model.x = -55
-          model.y = -80
-          app.renderer.view.style.position = 'absolute'
-          app.renderer.view.style.display = 'block'
-          app.renderer.autoResize = true
-          app.renderer.resize(window.innerWidth, window.innerHeight)
+          // model.x = -55
+          // model.y = -80
+          // app.renderer.view.style.position = 'absolute'
+          // app.renderer.view.style.display = 'block'
+          // app.renderer.autoResize = true
+          // app.renderer.resize(window.innerWidth, window.innerHeight)
           app.stage.addChild(model)
           model.scale.set(0.045) // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
           model.motion('JUMP_OUT');
@@ -223,51 +186,52 @@ function changModel(newId) {
       model.motion('JUMP_BACK');      
 }
 //禁止右击
-document.addEventListener('contextmenu', (event) => {
-  event.preventDefault()
-  console.log('右击---->')
-  window.chrome.webview.hostObjects.csobj.RightClick()
-})
-window.chrome.webview.addEventListener('message', (arg) => {
+// document.addEventListener('contextmenu', (event) => {
+//   event.preventDefault()
+//   console.log('右击---->')
+//   window.chrome.webview.hostObjects.csobj.RightClick()
+// })
+// window.chrome.webview.addEventListener('message', (arg) => {
 
-  console.log(arg.data);
-  var motionExt = arg.data['motion']
-  if(motionExt=='CHANGE_MODEL'){
-    var id = arg.data['motion_group_index'];
-    if(id<0)return;
-    changModel(id);
-    return;
-  }
+//   console.log(arg.data);
+//   var motionExt = arg.data['motion']
+//   if(motionExt=='CHANGE_MODEL'){
+//     var id = arg.data['motion_group_index'];
+//     if(id<0)return;
+//     changModel(id);
+//     return;
+//   }
 
-  var exp = arg.data['expression']
-  if (!exp) {
-    exp = 'NORMAL'
-  }
-  console.log('执行表情 ： ' + exp)
-  model.expression('Reset')
-  model.expression(exp)
-  //if(motionExt=='CENTER_Breathing') return;
+//   var exp = arg.data['expression']
+//   if (!exp) {
+//     exp = 'NORMAL'
+//   }
+//   console.log('执行表情 ： ' + exp)
+//   model.expression('Reset')
+//   model.expression(exp)
+//   //if(motionExt=='CENTER_Breathing') return;
 
-  if (motionExt) {
-    if (currentMotion.includes('RIGHT') || currentMotion.includes('LEFT')) {
-      if (motionExt.includes('RIGHT') || motionExt.includes('LEFT')) {
-        model.motion('HeadReset', undefined, MotionPriority.FORCE)
-        console.log('head expression reset')
-      } else {
-        model.motion('Reset', undefined, MotionPriority.FORCE)
-        console.log('DragUp  Reset')
-      }
-    } else {
-      console.log('Reset')
-      model.motion('Reset', undefined, MotionPriority.FORCE)
-    }
-    console.log('执行动画 ： ' + motionExt)
-    model.motion(motionExt, undefined, MotionPriority.FORCE)
-    currentMotion = motionExt
-  }
-})
-</script> <style>
+//   if (motionExt) {
+//     if (currentMotion.includes('RIGHT') || currentMotion.includes('LEFT')) {
+//       if (motionExt.includes('RIGHT') || motionExt.includes('LEFT')) {
+//         model.motion('HeadReset', undefined, MotionPriority.FORCE)
+//         console.log('head expression reset')
+//       } else {
+//         model.motion('Reset', undefined, MotionPriority.FORCE)
+//         console.log('DragUp  Reset')
+//       }
+//     } else {
+//       console.log('Reset')
+//       model.motion('Reset', undefined, MotionPriority.FORCE)
+//     }
+//     console.log('执行动画 ： ' + motionExt)
+//     model.motion(motionExt, undefined, MotionPriority.FORCE)
+//     currentMotion = motionExt
+//   }
+// })
+</script> 
+<!-- <style>
 ::-webkit-scrollbar {
   display: none;
 }
-</style> 
+</style>  -->
